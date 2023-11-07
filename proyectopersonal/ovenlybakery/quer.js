343cf7eaa82411ec35b26214d18e224a785661ab
FULLSTACK <miriamschzg@gmail.com>
2023-11-07 19:07:43 +0100

    valencia

--- proyectopersonal/ovenlybakery/quer.js
+++ proyectopersonal/ovenlybakery/quer.js
@@ -22,3 +22,27 @@ fichas.forEach((estiloFicha) => {
         estiloFicha.querySelector('.cuerpoFicha').classList.toggle('active')
     });
 });
+
+//TEXTO BEATING
+const beatingText = document.getElementById('beatingText');
+let scale = 1;
+let scalingUp = true;
+
+function updateBeatingText() {
+  if (scalingUp) {
+    scale += 0.002; //ajusta la velocidad a la que aumenta la escala
+    if (scale >= 1.05) { //tamaño máximo
+      scalingUp = false;
+    }
+  } else {
+    scale -= 0.002;
+    if (scale <= 0.95) { //tamaño mínimo
+      scalingUp = true;
+    }
+  }
+  
+  beatingText.style.transform = `scale(${scale})`;
+  requestAnimationFrame(updateBeatingText);
+}
+
+updateBeatingText();
